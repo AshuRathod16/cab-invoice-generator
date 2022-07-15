191869d7ebe0aaf8c7f37f2afb7b72c0c0ba3ae9
Ashwini Rathod <rashwini510@gmail.com>
2022-07-15 17:17:07 +0530

    Added: Welcome to cab invoice program

---
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+
+public class InvoiceService {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to cab invoice generator program");
+    }
+}
---
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertTrue;
+
+public class InvoiceServiceTest
+{
+
+}
