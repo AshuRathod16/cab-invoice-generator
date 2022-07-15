8ea112c194faa9a7ddf3109b368b3ccb3c6dbb09
Ashwini Rathod <rashwini510@gmail.com>
2022-07-15 18:18:51 +0530

    Modify: Calculate total fare

--- src/main/java/com/bridgelabz/InvoiceService.java
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -4,9 +4,11 @@ package com.bridgelabz;
 public class InvoiceService {
     private static final double COST_PER_KM = 10;
     private static final double COST_PER_MIN = 1;
+    private static final double MIN_Fare = 5;
 
     public double calculateFare(double distance, int time) {
 
-        return distance * COST_PER_KM + time * COST_PER_MIN;
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_Fare);
     }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -14,4 +14,13 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25, totalFare, 0.0);
     }
 
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5, totalFare, 0.0);
+    }
+
 }
